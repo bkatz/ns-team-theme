e8278f11b6c289f2510c08642f81680020273214
Benjamin Katz <bk@benjaminkatzcreative.com>
2022-04-16 13:11:10 -0700

    add ipad browser support

--- admin/theme/theme-setup.php
+++ admin/theme/theme-setup.php
@@ -170,7 +170,7 @@ function wpst_browser_classes($classes)
 {
     // the list of WordPress global browser checks
     // https://codex.wordpress.org/Global_Variables#Browser_Detection_Booleans
-    $browsers = ['is_iphone', 'is_chrome', 'is_safari', 'is_NS4', 'is_opera', 'is_macIE', 'is_winIE', 'is_gecko', 'is_lynx', 'is_IE', 'is_edge'];
+    $browsers = ['is_iphone', 'is_chrome', 'is_safari', 'is_NS4', 'is_opera', 'is_macIE', 'is_winIE', 'is_gecko', 'is_lynx', 'is_IE', 'is_edge', 'is_ipad'];
 
     // check the globals to see if the browser is in there and return a string with the match
     $classes[] = join(' ', array_filter($browsers, function ($browser) {
